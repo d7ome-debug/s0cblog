1f81c0926184f69cd6b2b2b2e667519ebc3ddace
d7ome-debug <d7ome121121@gmail.com>
2024-01-21 22:51:24 +0300

    a

--- socblog/settings.py
+++ socblog/settings.py
@@ -86,6 +86,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
