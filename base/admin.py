b3f0670b869b9acd546ba035ab420dc44a90c25e
d7ome-debug <d7ome121121@gmail.com>
2024-01-23 23:09:10 +0300

    asdf

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Feature, Post, Comment, View, likes
+from .models import Feature, Post, Comment, View
 admin.site.register(Feature)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(View)
-admin.site.register(likes)
